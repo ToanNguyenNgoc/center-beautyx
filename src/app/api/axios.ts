d1748a12f743f610b3fd745784996c2a20b379b9
nguyentoan06 <toan@myspa.vn>
2023-06-16 11:34:42 +0700

    [UPDATE] api live

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-export const baseURL = process.env.REACT_APP_API_URL_DEV;
-// export const baseURL = process.env.REACT_APP_API_URL
+// export const baseURL = process.env.REACT_APP_API_URL_DEV;
+export const baseURL = process.env.REACT_APP_API_URL
 const axiosClient = axios.create({
   baseURL: baseURL,
   headers: {
